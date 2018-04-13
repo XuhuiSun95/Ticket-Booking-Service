579cbea130da5cb1d83a53861918e6cc43ad44be
XuhuiSun95 <ericsun1995@gmail.com>
2018-04-13 10:46:08 -0700

    update documentation

--- server/ServerSocket.cpp
+++ server/ServerSocket.cpp
@@ -11,7 +11,6 @@ ServerSocket::~ServerSocket() {
     close(mSockfd);
 }
 
-//function used to initialize socket
 void ServerSocket::Init(const std::string &ipaddress, const int &port, bool host) {
 
     if((mSockfd = socket(AF_INET, SOCK_STREAM, 0))==0) {
@@ -27,12 +26,13 @@ void ServerSocket::Init(const std::string &ipaddress, const int &port, bool host
     if(host) {
 
         mAddress.sin_addr.s_addr = INADDR_ANY;
-        // call bind to associate the socket with our local address and
-        // port
+
+        // call bind to associate the socket with our local address and port
         if(bind(mSockfd, (struct sockaddr *)&mAddress, sizeof(mAddress))<0) {
             std::cout << "Bind Failed" << std::endl;
             return;
         }
+
         // convert the socket to listen for incoming connections
         if(listen(mSockfd, 10)<0) {
             std::cout << "Listen Failed" << std::endl;
@@ -40,7 +40,7 @@ void ServerSocket::Init(const std::string &ipaddress, const int &port, bool host
         }
     }
     else {
-        
+
         if(inet_pton(AF_INET, ipaddress.c_str(), &mAddress.sin_addr)<=0) {
             std::cout << "Invalid pair server address/ Address not supported" << std::endl;
             return;
@@ -60,12 +60,11 @@ bool ServerSocket::Valid() {
     return mValid;
 }
 
-//function used to accept clients, return number greater than 0 if success
 int ServerSocket::Accept() {
 
     int addrlen = sizeof(mAddress);
     int new_socket;
-    
+
     if((new_socket = accept(mSockfd, (struct sockaddr *)&mAddress, (socklen_t*)&addrlen))<0) {
         std::cout << "Accept Failed" << std::endl;
         return -1;
@@ -74,7 +73,6 @@ int ServerSocket::Accept() {
     return new_socket;
 }
 
-//function used to read message sent from client.
 std::string ServerSocket::GetMessage(const int &sock) {
 
     mValread = read(sock, buffer, 1024);
@@ -84,7 +82,6 @@ std::string ServerSocket::GetMessage(const int &sock) {
     return message;
 }
 
-//function used to send back message to client
 void ServerSocket::SendMessage(const int &sock, const std::string &message) {
 
     send(sock, message.c_str(), message.length(), 0);
@@ -92,9 +89,6 @@ void ServerSocket::SendMessage(const int &sock, const std::string &message) {
     close(sock);
 }
 
-//function used to forward message to other server
-//if the requested type ticket is sold on other server
-//return message get back from other server
 std::string ServerSocket::ForwardTicket(const std::string &message) {
 
     send(mSockfd, message.c_str(), message.length(), 0);
--- server/ServerSocket.hpp
+++ server/ServerSocket.hpp
@@ -13,14 +13,28 @@ class ServerSocket {
 
 public:
 
+    //  ServerSocket Constructor
     ServerSocket();
+    //  ServerSocket Destructor
     ~ServerSocket();
 
+    //  Function used to initialize socket
+    //  @param ipaddress The socket IP address
+    //  @param port The socket port number
+    //  @param host True if create a host socket, otherwise create a forward socket
     void Init(const std::string &ipaddress, const int &port, bool host=true);
+    //  Check if Socket Init() correctly
     bool Valid();
+    //  Function used to accept clients
+    //  return the socket file descriptor
     int Accept();
+    //  Function used to read message sent from client
     std::string GetMessage(const int &sock);
+    //  Function used to send back message to client
     void SendMessage(const int &sock, const std::string &message);
+    //  Function used to forward message to other server
+    //  if the requested type ticket is sold on other server
+    //  return message get back from other server
     std::string ForwardTicket(const std::string &message);
 
 private:
